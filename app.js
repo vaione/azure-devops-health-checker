46aa76b69da8389f1208d86211303633a01600c4
luke-h1 <luke.howsam@yahoo.com>
2020-09-21 19:17:10 +0100

    add loading icon that is reusable

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const BASE_URL = `https://cors-anywhere.herokuapp.com/https://status.dev.azure.c
 const gridOutput = document.getElementById('grid');
 const generalStatusOutput = document.getElementById('general-status');
 
+function showLoader(message) {
+  const loader = document.getElementById('loading-img');
+  if (message === 'true') {
+    loader.style.display = 'block';
+  } else if (message === 'false') {
+    loader.style.display = 'none';
+  }
+}
+
 async function fetchGeneralStatus() {
+  showLoader('true');
   const response = await fetch(BASE_URL);
   const json = await response.json();
   showStatus(json);
 }
 
 function showStatus(json) {
+  showLoader('false');
   let statusOutput = '';
   statusOutput += `
   <p>LAST UPDATED: ${json.lastUpdated} </p>
@@ -19,7 +30,8 @@ function showStatus(json) {
       : `<span class="healthy">${json.status.health}</span>`
   }</p>
 <p>Message: ${
-    json.status.message === 'Service maintenance is in effect'
+    json.status.message === 'Service maintenance is in effect' ||
+    'Availability Degradation'
       ? `<span class="unhealthy">${json.status.message}</span>`
       : `<span class="healthy">${json.status.message}</span>`
   }  </p>
@@ -28,12 +40,15 @@ function showStatus(json) {
 }
 
 async function fetchStatus() {
+  showLoader('true')
   const res = await fetch(BASE_URL);
   const data = await res.json();
   showDataDOM(data);
 }
 
 function showDataDOM(data) {
+  showLoader('false')
+
   let output = '';
   data.services.forEach((item, i) => {
     output += `
