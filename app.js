e27a50dda9fb5318cc6ec35731b9a01f84d8e862
luke-h1 <luke.howsam@yahoo.com>
2020-09-21 18:54:03 +0100

    start to format the response the api spits out

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById('btn');
 const BASE_URL = `https://status.dev.azure.com/_apis/status/health?api-version=6.0-preview.1`;
-const gridOutput = document.getElementById('grid')
+const gridOutput = document.getElementById('grid');
 async function fetchStatus() {
   const res = await fetch(BASE_URL);
   const data = await res.json();
@@ -9,14 +9,19 @@ async function fetchStatus() {
 
 function showDataDOM(data) {
   let output = '';
-  data.services.forEach((item) => {
+  data.services.forEach((item, i) => {
     output += `
   <div class="card">
   <p>id:</p>
   <ul class="list">
-    <li>${item.id}</li>
-    <li>${item.geographies.name}</li>
-    <li>${item.geographies.health}</li>
+    <li> SERVICE: ${item.id}</li>
+    <li> COUNTRY: ${item.geographies[i].name}</li>
+    <li> REGION: ${item.geographies[i].id}</li>
+    <li> STATUS: ${
+      item.geographies[i].health === 'healthy'
+        ? `<span class='healthy'>${item.geographies[i].health}</span>`
+        : `<span class='unhealthy'>${item.geographies[i].health}</span>`
+    }</li>
   </ul>
 </div>
   
